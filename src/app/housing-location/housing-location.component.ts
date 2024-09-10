32a163792bf1ead02f2a906631b01e417782b37f
iasif <imtiazasif.cse@gmail.com>
2024-09-10 12:34:41 +0600

    showing a single component

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,20 +1,62 @@
-import { Component, Input } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { HousingLocation } from "../housingLocation";
+// import { Component, Input } from "@angular/core";
+// import { CommonModule } from "@angular/common";
+// import { HousingLocation } from "../housingLocation";
+
+// @Component({
+//   selector: "app-housing-location",
+//   standalone: true,
+//   imports: [CommonModule],
+//   template: `
+//     <!-- <section class="listing">
+//       <img class="listing-photo" [src]="HousingLocation.photo"  alt= "Exterior photo of {{HousingLocation.name}} " />
+//       <h2 class="lisiting-heading">{{HousingLocation.name}} </h2>
+//       <p class="listing-location"> {{HousingLocation.city}} {{HousingLocation.state}} </p>
+//     </section> -->
+    
+//     <section>HOUSLOCATION IS WORKING</section>
+//     <section class="listing">
+//       <img
+//         class="listing-photo"
+//         [src]="housingLocation.photo"
+//         alt="Exterior photo of {{ housingLocation.name }}"
+//         crossorigin
+//       />
+//       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+//       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+//     </section>
+  
+//   `,
+//   styleUrls: ["./housing-location.component.css"],
+// })
+// export class HousingLocationComponent {
+//   @Input() housingLocation!: HousingLocation;
+// }
+
+
+
+////////////////////
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocation} from '../housingLocation';
 
 @Component({
-  selector: "app-housing-location",
+  selector: 'app-housing-location',
   standalone: true,
   imports: [CommonModule],
   template: `
     <section class="listing">
-      <img class="listing-photo" [src]="HousingLocation.photo"  alt= "Exterior photo of {{HousingLocation.name}} " />
-      <h2 class="lisiting-heading">{{HousingLocation.name}} </h2>
-      <p class="listing-location"> {{HousingLocation.city}} {{HousingLocation.state}} </p>
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location"> {{ housingLocation.city }}, {{ housingLocation.state }}</p>
     </section>
   `,
-  styleUrls: ["./housing-location.component.css"],
+  styleUrls: ['./housing-location.component.css'],
 })
 export class HousingLocationComponent {
-  @Input() HousingLocation!: HousingLocation;
+  @Input() housingLocation!: HousingLocation;
 }
